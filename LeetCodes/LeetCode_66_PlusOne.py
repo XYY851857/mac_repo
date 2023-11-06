4fce8292a734526812d5f71394a31fd2bf6daf97
XYY <xyy@XYYs-MacBook-Air.local>
2023-11-06 11:25:16 +0800

    20231103 mac

---
+++ LeetCodes/LeetCode_66_PlusOne.py
@@ -0,0 +1,29 @@
+"""
+[十進位換算]
+給一包含非數整數的陣列，其中每一個值代表該位數的值，對這個陣列加1。
+範例：
+19 = [1,9] ， 19+1 = 20 = [2,0]。
+"""
+
+
+def algo(list1):
+    i = 0
+    list1[-1] += 1
+    if list1[0] == 9:
+        list1.insert(0, 0)
+    while i < len(list1):
+        if list1[-(i + 1)] == 10:
+            list1[-(i + 2)] += 1
+            list1[-(i + 1)] = 0
+        i += 1
+    return list1
+
+
+if __name__ == "__main__":
+    """
+    num = int(input("input : "))
+    list1 = [int(digit) for digit in str(num)]
+    """
+    list1 = [9, 9, 9]
+    ans = algo(list1)
+    print(ans)
---
+++ SandBox/SandBox1.py
@@ -0,0 +1,5 @@
+i = 0
+list_a = [1, 2, 3, 4]
+while i < len(list_a):
+    print(list_a[-(i+1)])
+    i += 1
