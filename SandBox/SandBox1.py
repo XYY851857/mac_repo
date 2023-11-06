4a08334c8fc4da8495a4957a8c3a9ddcea176077
XYY <xyy@XYYs-MacBook-Air.local>
2023-11-06 14:45:30 +0800

    20231103 mac

--- SandBox/SandBox1.py
+++ SandBox/SandBox1.py
@@ -1,5 +1,21 @@
-i = 0
-list_a = [1, 2, 3, 4]
-while i < len(list_a):
-    print(list_a[-(i+1)])
-    i += 1
+def algo(list1):
+    i = 0
+    list1[-1] = int(list1[-1]) + 1
+    if list1[0] == "2":
+        list1[0] = "0"
+        list1.insert(0, "1")
+    while i < len(list1):
+        if list1[-(i + 1)] == "2":
+            list1[-(i + 1)] = "0"
+            list1[-(i + 2)] = str(int(list1[-(i + 2)]) + 1)
+        i += 1
+    return list1
+
+
+if __name__ == "__main__":
+    num = int(input("請輸入一個數字："))
+    list1 = [int(digit) for digit in str(num)]
+    ans = algo(list1)
+    ans = [str(digit) for digit in ans]  # 轉換元素為字元字串
+    print("結果:", ''.join(ans))
+
