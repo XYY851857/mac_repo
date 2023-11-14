f252aebbf1ca36e73c9e380574a8998e6e48b7da
XYY851857 <149142422+XYY851857@users.noreply.github.com>
2023-11-15 02:23:56 +0800

    20231114 MAC

--- LeetCodes/LeetCode_5_LongestPalindromicSubstring.py
+++ LeetCodes/LeetCode_5_LongestPalindromicSubstring.py
@@ -1,3 +1,18 @@
+"""
+給定一個字串s，傳回最長的 回文的子字串在s.
+範例1：
+輸入： s = "babad"
+輸出： "bab"
+解釋： "aba" 也是有效答案。
+範例2：
+輸入： s = "cbbd"
+輸出： "bb"
+限制條件：
+1 <= s.length <= 1000
+s僅由數字和英文字母組成。
+"""
+
+
 class Solution:
     def __init__(self, s):
         self.s = s
@@ -10,8 +25,9 @@ class Solution:
                     data.append(''.join(self.s[i: j + 1]))
         return data
 
+
 if __name__ == "__main__":
     s = 'snfoawneiofnaw'
-    solution_instance = Solution(s)
-    ans = solution_instance.algo()
+    solution = Solution(s)
+    ans = solution.algo()
     print(ans)
