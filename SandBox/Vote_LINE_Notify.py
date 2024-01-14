1a18c1a242808dab6ab7b5bc0eba555843b803a1
XYY851857 <149142422+XYY851857@users.noreply.github.com>
2024-01-14 15:09:22 +0800

    V2 1508, 已修改為可正確抓取資料 20240113 from XYY's Mac

--- SandBox/Vote_LINE_Notify.py
+++ SandBox/Vote_LINE_Notify.py
@@ -1,3 +1,4 @@
+import re
 import time
 import traceback
 import requests
@@ -14,20 +15,35 @@ def get(pack1):
     html = requests.get(url, headers=chrome_headers)
 
     soup = BeautifulSoup(html.text, 'html.parser')
-    data = soup.find_all('td', class_="Bxz(bb) Px(24px) Px(4px)--md1019 Px(4px)--sm767 Py(12px) Ta(end) Fxb(0) Fxg(1) Fxs(1)")
-    for step in range(0, len(data), 3):
-        kp += int(data[step].text.strip().replace(',', ''))
-        lai += int(data[step+1].text.strip().replace(',', ''))
-        hou += int(data[step+2].text.strip().replace(',', ''))
+    data = soup.find_all('tr', class_="D(f) Fz(16px) Fz(14px)--sm767 Fz(12px)!--mbsm4 Lh(24px) Lh(20px)--sm767 Fw(400) C(#232a31) Bdbw(1px) Bdbs(s) Bdbc(#e0e4e9)")
+    # print(data[0].text.strip().replace(',', '').split('票'))
+    for step in range(0, len(data)):
+        cache = re.split(r'[票市縣]', data[step].text.strip().replace(',', ''))
+        # print(cache)
+        # kp = int(cache[1])
+        # lai = int(cache[2])
+        # hou = int(cache[3])
+        # print(f'kp:{kp}    lai:{lai}    hou:{hou}')
+        kp += int(cache[1])
+        lai += int(cache[2])
+        hou += int(cache[3])
+        # print(f'kp:{kp}    lai:{lai}    hou:{hou}')
+
+        # kp = int(data[step].text.strip().replace(',', '').replace('票', ''))
+        # print('kp', kp)
+        # lai = int(data[step+1].text.strip().replace(',', '').replace('票', ''))
+        # print('lai', lai)
+        # hou = int(data[step+2].text.strip().replace(',', '').replace('票', ''))
+        # print('hou', hou)
     return kp, lai, hou
 
 
 def notify(data):
     kp, lai, hou = data
     url = "https://notify-api.line.me/api/notify"
-    token = "OUZVMu567J4aJTV3ttYCaA2pc7vlpqX86xeH7Emm3u1"
+    token = "O22XmpnxuecnSEFPJl01cKFQBhDMy7Omn1RMXjLwiiq"  # TEST Token
     headers = {"Authorization": "Bearer " + token}
-    message = f'\n時間：{datetime.now().strftime("%Y-%m-%d    %H:%M:%S")}\n柯文哲/吳欣盈：{kp} 票\n賴清德/蕭美琴：{lai} 票\n侯友宜/趙少康：{hou} 票\n總票數：{kp + lai + hou} 票'
+    message = f'\n時間：{datetime.now().strftime("%Y-%m-%d    %H:%M:%S")}\n柯文哲/吳欣盈：{kp} 票\n賴清德/蕭美琴：{lai} 票\n侯友宜/趙少康：{hou} 票\n總票數：{kp + lai + hou} 票\n**票數僅供參考**'
 
     data = {"message": message}
     resp = requests.post(url, headers=headers, data=data)
@@ -45,10 +61,12 @@ if __name__ == "__main__":
             pack1 = (kp, lai, hou, url)
             data = get(pack1)
             resp = notify(data)
-            time.sleep(30)
+            break
+            # time.sleep(60)
         except Exception as e:
             traceback.print_exc()
             print(e)
-            report(__file__, e)
-            time.sleep(15)
+            # report(__file__, e)
+            break
+            # time.sleep(15)
 
